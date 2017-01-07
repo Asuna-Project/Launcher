565789a769fef3c4a3b03345ffa6c7dc00d6bf52
Soundofdarkness <imsoundoflight@gmail.com>
2017-01-07 18:15:32 +0100

    Test Fixed

--- src/test/java/DownloaderTest.java
+++ src/test/java/DownloaderTest.java
@@ -4,6 +4,8 @@ import com.asuna.launcher.utils.Downloader;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -12,18 +14,29 @@ import static org.junit.jupiter.api.Assertions.fail;
  */
 public class DownloaderTest {
     @Test
-    public void testDownloader(){
+    public void testDownloader() {
+        Logger LOGGER = Logger.getLogger("DownloadTest");
         try {
             Downloader.download(new URL("https://raw.githubusercontent.com/Asuna-Project/core/master/README.md"),
                     new File("./Readme.MD"));
-            return;
-        }
-        catch (Exception ex){
+
+        } catch (Exception ex) {
             fail("Downloader Failed with " + ex.getMessage());
         }
-        File file = new File("./Readme.MD");
-        file.delete();
+        try {
+            File file = new File("./Readme.MD");
+            if (file.delete()) {
+                LOGGER.log(Level.INFO, file.getName() + " is deleted!");
+                return;
+            } else {
+                LOGGER.log(Level.INFO, "Delete operation is failed.");
+                fail("Failed to delete Test File");
+            }
+        }
+        catch(Exception e){
 
+        fail("Failed with Deleting File " + e.getMessage());
+    }
 
     }
 }
